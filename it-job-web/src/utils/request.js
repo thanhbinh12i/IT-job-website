e7445beafa9f349eb4e05fe01b843c111d641fc5
thanhbinh12ii <nptbinh17092004@gmail.com>
2024-10-20 19:49:22 +0700

    update api

--- it-job-web/src/utils/request.js
+++ it-job-web/src/utils/request.js
@@ -1,4 +1,4 @@
-const API_DOMAIN = "http://localhost:3002/";
+const API_DOMAIN = "https://database-it-job-website.vercel.app/";
 
 export const get = async (path) =>{
       const response = await fetch(API_DOMAIN + path);
